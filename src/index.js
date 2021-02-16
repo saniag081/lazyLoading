7c3a88fc1c6222b784ddcc0ef5b3552755b552a5
saniag081 <sgonzalezhernandez081@gmail.com>
2021-02-15 22:07:18 -0500

    added log images,  shape image

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {registerImage} from './lazy'
 
 const app = document.querySelector('#app')
 const btnAddImage = document.querySelector('#addImage')
+const btnClean = document.querySelector('#btnClean')
 const maxImage = 122
 const minImage = 1
 
@@ -11,11 +12,11 @@ function numberRandom () {
 
 function createImageNode() {
 	const container = document.createElement('div')
-	container.className = 'p-4'
+	container.className = 'p-4 c-shape'
 	const elementImage = document.createElement('img')
 	elementImage.className = 'mx-auto'
 	elementImage.width = '320'
-	elementImage.src = `https://randomfox.ca/images/${numberRandom()}.jpg`
+	elementImage.dataset.src = `https://randomfox.ca/images/${numberRandom()}.jpg`
 
 	container.appendChild(elementImage)
 	return container
@@ -27,5 +28,9 @@ function addedImage () {
 	registerImage(newImage)
 }
 
+function cleanApp() {
+	app.innerHTML = ''
+}
 
+btnClean.addEventListener('click', cleanApp)
 btnAddImage.addEventListener('click',addedImage)
--- src/lazy.js
+++ src/lazy.js
@@ -1,17 +1,33 @@
+let loadImages = 0
+let totalIamges = 0
+
+function logImages() {
+	console.log('=============================')
+	console.log(`Total Images: ${totalIamges}`)
+	console.log(`Imagenes cargadas: ${loadImages}`)
+	console.log('=============================')
+}
+
 function isInterecting(entry) {
 	return entry.isIntersecting
 }
 
-function accion(entry) {
-	const nodo = entry.target
-	console.log('even')
-	observer.unobserve(nodo)
+function loadImage(entry) {
+	loadImages++
+	const container = entry.target
+	const elementImage = container.firstChild
+	const url =  elementImage.dataset.src
+	elementImage.src = url
+	observer.unobserve(container)
+	logImages()
 }
 
 const observer = new IntersectionObserver((entries) => {
-	entries.filter(isInterecting).forEach(accion)
+	entries.filter(isInterecting).forEach(loadImage)
 })
 
 export function registerImage(image) {
+	logImages()
+	totalIamges++
 	observer.observe(image)
 }
